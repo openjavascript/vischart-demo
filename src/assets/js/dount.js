6ef5c0f445ec6ae312305360d91bcd7c1d678548
qiushaowei <qiushaowei@360.net>
2018-09-29 17:06:07 +0800

    up

--- src/assets/js/dount.js
+++ src/assets/js/dount.js
@@ -11,7 +11,8 @@ font.load().then(function () {
 
     let data = require( './data/dount.json' );
     let dmins = new VisChart( document.querySelector('#twoBox'), 340, 340 );
-        dmins.setImage( './img/dount-out.png', 120, 120, 0, 1 );
+        dmins.addImage( './img/dount-out.png', 120, 120, 0, 1 );
+        dmins.addImage( './img/dount-big.png', 250, 248, 0, 1 );
         dmins.update( data ); 
 
         setTimeout( ()=>{

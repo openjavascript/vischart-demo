b4976ba68d77c44fb0ff019c902b5650f817f697
qiushaowei <qiushaowei@360.net>
2018-09-29 16:43:33 +0800

    up

--- src/assets/js/dount.js
+++ src/assets/js/dount.js
@@ -2,13 +2,25 @@
 
 import VisChart from 'vischart';
 
+import fontfaceobserver from 'fontfaceobserver';
 
-let data = require( './data/dount.json' );
 
-let dmins = new VisChart( document.querySelector('#twoBox'), 340, 340 );
-    dmins.setImage( './img/dount-out.png', 120, 120, 0, 1 );
-    dmins.update( data ); 
 
-    setTimeout( ()=>{
-        //dmins.update( data ); 
-    }, 5000 );
+let font = new fontfaceobserver( 'HuXiaoBoKuHei' );
+font.load().then(function () {
+
+    let data = require( './data/dount.json' );
+    let dmins = new VisChart( document.querySelector('#twoBox'), 340, 340 );
+        dmins.setImage( './img/dount-out.png', 120, 120, 0, 1 );
+        dmins.update( data ); 
+
+        setTimeout( ()=>{
+            //dmins.update( data ); 
+        }, 5000 );
+
+}, function () {
+  console.log('Font is not available');
+});
+
+
+
